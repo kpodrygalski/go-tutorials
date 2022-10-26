10d29b86c71f4cd7dfdf98b14c2c31b1e9b9f1a0
Konrad Podrygalski <75859974+kpodrygalski@users.noreply.github.com>
2022-10-26 17:14:14 +0200

    Created new tutorial package
    Created package about values in Go

---
+++ 02-values/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello" + " World") // Stringi mogą być łączone poprzez znak +
+	fmt.Println("1+5=", 1+5) // Liczby całkowite (integer) i liczby zmiennoprzecinkowe (float)
+	fmt.Println(true && false) // Typ Prawda / Fałsz za pomocą operatorów logicznych
+	fmt.Println(true || false) 
+}
